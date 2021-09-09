dad5d9a5f657a87a25633706fdb508c4d0024430
Michael <80859948+bridgemami@users.noreply.github.com>
2021-09-09 00:31:55 +0000

    onto final

--- nextjs-blog/nextjs-blog/nextjs-blog/pages/index.js
+++ nextjs-blog/nextjs-blog/nextjs-blog/pages/index.js
@@ -2,17 +2,15 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
   return (
-     <Layout home>
+    <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello!</p>
+        <p>[Your Self Introduction]</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
@@ -31,7 +29,7 @@ export default function Home({ allPostsData }) {
     </Layout>
   )
 }
-  
+
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
   return {
