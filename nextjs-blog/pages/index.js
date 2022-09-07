2d7f6e108dbf537764d2f2caf107addbf755d113
bridgemami <mdbridgeman@gmail.com>
2022-09-07 12:59:39 -0700

    finished Navigate Between Pages

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Head from 'next/head'
 
 export default function Home() {
@@ -9,9 +10,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className="title">
+  Read <Link href="/posts/first-post">this page!</Link>
+</h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
