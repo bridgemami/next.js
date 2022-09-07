c38fa48e3a657736e0fee7398756c42903add38a
bridgemami <mdbridgeman@gmail.com>
2022-09-07 14:31:25 -0700

    added my name

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,25 +1,25 @@
-import utilStyles from '../../styles/utils.module.css';
-import Head from 'next/head';
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
-import Date from '../../components/date';
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
-    return (
-      <Layout>
-        <Head>
-          <title>{postData.title}</title>
-        </Head>
-        <article>
-          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-          <div className={utilStyles.lightText}>
-            <Date dateString={postData.date} />
-          </div>
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </article>
-      </Layout>
-    );
-  }
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
@@ -30,12 +30,10 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-    // Add the "await" keyword like this:
-    const postData = await getPostData(params.id);
-  
-    return {
-      props: {
-        postData,
-      },
-    };
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
   }
+}
